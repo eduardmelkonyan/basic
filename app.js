c1778471374897c05a1dc022a3e693cbab757729
eduardmelkonyan <melkon1999@gmail.com>
2021-10-05 11:09:23 +0400

    get all users

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const User = require('./users/index');
+const Subscribtion = require('./subscribtion/index');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const user = new User();
+const subscribtion = new Subscribtion();
+
+
+app.post('/reg', async (req, res) => {
+  try {
+    console.log(req.body);
+    const { password, email } = req.body;
+
+    if(!password, !email) {
+      return res.send('Invalid registration data');
+    }
+
+    const result = await user.create(req.body);
+
+    return res.send(result);
+  } catch(e) {
+    return res.send('Something went wrong');
+  }
+});
+
+app.post('/login', async (req, res) => {
+
+  try {
+    const { email, password } = req.body;
+    if(!email || !password) {
+    return res.send('Invalid login data');
+    }
+
+  const result = await user.login(email, password);
+  return res.send(result);
+
+  } catch(e) {
+    return res.send('Something went wrong');
+  }
+});
+
+app.post('/logOut', async (req, res) => {
+
+  try {
+    const { token } = req.body;
+    if(!token) {
+    return res.send('Invalid logOut data or token');
+    }
+
+  const result = await user.logOut(token);
+  return res.send(result);
+
+  } catch(e) {
+      return res.send('Something went wrong');
+  }
+});
+
+app.post('/subscribtions/:id', async (req, res) => {
+
+  try {
+    const { token } = req.body;
+    const target = req.params.id;
+    const currentUser = await user.checkLogin(token);
+    const result = await subscribtion.create(currentUser, target);
+    console.log(result);
+    return res.send(result);
+  } catch(e) {
+    return res.send('Something went wrong');
+  }
+});
+
+app.get('/subscribtions', async (req, res) => {
+
+  try {
+    const { token } = req.body;
+    const currentUser = await user.checkLogin(token);
+    const result = await subscribtion.getPnadingSubscriptions(currentUser);
+    return res.send(result);
+  } catch(e) {
+    console.log(e);
+    return res.send('Something went wrong');
+  }
+});
+
+app.put('/subscribtions/:id', async (req, res) => {
+  try {
+    const { token } = req.body;
+    const currentUser = await user.checkLogin(token);
+    const subscriptionId = req.params.id;
+    const result = await subscribtion.acceptPandingSubscription(currentUser, subscriptionId);
+    return res.send(result);
+  } catch(e) {
+    console.log(e);
+    return res.send('Something went wrong');
+  }
+});
+
+app.delete('/subscribtions/:id', async (req, res) => {
+  try{
+    const { token } = req.body;
+    const currentUser = await user.checkLogin(token);
+    const subscriptionId = req.params.id;
+    const result = await subscribtion.rejectPandingSubscribtions(currentUser, subscriptionId);
+    return res.send(result);
+  } catch(e) {
+    console.log(e);
+    return res.send('Something went wrong');
+  }
+});
+
+app.get ('/users', async (req, res) => {
+ try{
+  const { token } = req.body;
+  const currentUser = await user.checkLogin(token);
+  console.log('Hello');
+  const result = await user.getUsers(currentUser);
+  return res.send(result);
+}catch(e){
+  console.log(e);
+  return res.send('Something went wrong')
+}
+});
+
+app.listen(3000);
---
+++ users/index.js
@@ -0,0 +1,82 @@
+const redis = require("redis");
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+
+const { UserModel } = require('../db/index');
+
+const client = redis.createClient();
+
+class User {
+  constructor() {};
+
+  async create(user) {
+
+    const findUser = await UserModel.findOne({ email: user.email });
+
+    if(findUser) {
+      throw new Error('This email is already exists');
+    }
+    const result = await UserModel.create(user);
+    return result;
+  }
+
+  async login(email, password) {
+
+    const user = await UserModel.findOne({ email, password });
+    if(!user) {
+      throw new Error('User not found!');
+    }
+    const token = jwt.sign({ userId: user.id }, 'basicItCenter');
+    console.log(token);
+    await promisify(client.set).bind(client)(user.id, token);
+    return { token };
+  }
+
+  async logOut(token) {
+
+    const { userId, iat } = jwt.verify(token, 'basicItCenter');
+    console.log('iattt', iat, userId);
+    const user = await UserModel.findOne({ _id: userId });
+    if(!user) {
+      throw new Error('Sonething went wrong');
+    }
+    await promisify(client.del).bind(client)(user.id);
+    return 'success'
+  }
+
+  async checkLogin(token) {
+    const { userId, iat } = jwt.verify(token, 'basicItCenter');
+    const user = await UserModel.findOne({ _id: userId });
+    const id = user._id;
+    const isLogined = await promisify(client.get).bind(client)(user._id.toString());
+    if(!user || !isLogined) {
+      throw new Error('This user not found!');
+    }
+
+    return user._id;
+  }
+
+  async find(id) {
+    const result = await UserModel.findOne({_id: id});
+    return result;
+  }
+
+  async getUsers (id) {
+    const currentUser = await UserModel.findOne({ _id: id });
+    console.log(currentUser);
+    if(!currentUser) {
+      throw new Error('User not found!');
+    }
+    const allUsers = await UserModel.find({ _id: { $ne: id } }).select([
+      "password",
+      "firstName",
+      "lastName",
+      "phone",
+      "subscriber",
+  ]);
+  return allUsers;
+  }
+}
+
+module.exports = User;
