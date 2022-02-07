7f2f010db20806925039496711459de0975322e8
Eduard <melkon1999@gmail.com>
2022-02-07 17:55:27 +0400

    updated

--- users/index.js
+++ users/index.js
@@ -1,72 +1,70 @@
 const redis = require("redis");
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
-
-const { UserModel } = require('../db/index');
+const { UserModel } = require("../db/index");
 
 const client = redis.createClient();
 
 class User {
-  constructor() {};
+  constructor() {}
 
   async create(user) {
-
     const findUser = await UserModel.findOne({ email: user.email });
 
-    if(findUser) {
-      throw new Error('This email is already exists');
+    if (findUser) {
+      throw new Error("This email is already exists");
     }
     const result = await UserModel.create(user);
     return result;
   }
 
   async login(email, password) {
-
     const user = await UserModel.findOne({ email, password });
-    if(!user) {
-      throw new Error('User not found!');
+    if (!user) {
+      throw new Error("User not found!");
     }
-    const token = jwt.sign({ userId: user.id }, 'basicItCenter');
+    const token = jwt.sign({ userId: user.id }, "basicItCenter");
     console.log(token);
     await promisify(client.set).bind(client)(user.id, token);
     return { token };
   }
 
   async logOut(token) {
-
-    const { userId, iat } = jwt.verify(token, 'basicItCenter');
-    console.log('iattt', iat, userId);
+    const { userId, iat } = jwt.verify(token, "basicItCenter");
+    console.log("iattt", iat, userId);
     const user = await UserModel.findOne({ _id: userId });
-    if(!user) {
-      throw new Error('Sonething went wrong');
+    if (!user) {
+      throw new Error("Sonething went wrong");
     }
     await promisify(client.del).bind(client)(user.id);
-    return 'success'
+    return "success";
   }
 
   async checkLogin(token) {
-    const { userId, iat } = jwt.verify(token, 'basicItCenter');
+    const { userId, iat } = jwt.verify(token, "basicItCenter");
     const user = await UserModel.findOne({ _id: userId });
     const id = user._id;
-    const isLogined = await promisify(client.get).bind(client)(user._id.toString());
-    if(!user || !isLogined) {
-      throw new Error('This user not found!');
+    const isLogined = await promisify(client.get).bind(client)(
+      user._id.toString()
+    );
+    if (!user || !isLogined) {
+      throw new Error("This user not found!");
     }
 
     return user._id;
   }
 
   async find(id) {
-    const result = await UserModel.findOne({_id: id});
+    const result = await UserModel.findOne({ _id: id });
     return result;
   }
 
-  async getUsers (id) {
+  async getUsers(id) {
     const currentUser = await UserModel.findOne({ _id: id });
     console.log(currentUser);
-    if(!currentUser) {
-      throw new Error('User not found!');
+    if (!currentUser) {
+      throw new Error("User not found!");
     }
     const allUsers = await UserModel.find({ _id: { $ne: id } }).select([
       "password",
@@ -74,8 +72,8 @@ class User {
       "lastName",
       "phone",
       "subscriber",
-  ]);
-  return allUsers;
+    ]);
+    return allUsers;
   }
 }
 
